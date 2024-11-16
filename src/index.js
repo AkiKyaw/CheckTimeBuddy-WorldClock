c5719ded82c74682ccdf865f360124a6fc1a8e63
EiEiKyaw <11eikyw@gmail.com>
2024-11-16 16:57:08 +0630

    update function

--- src/index.js
+++ src/index.js
@@ -5,17 +5,32 @@ let YgnTimeElement = document.querySelector("#ygnTime");
 let YgnDateElement = document.querySelector("#ygnDate");
 let YgnAMElement = document.querySelector("#ygnAM");
 YgnDateElement.innerHTML = YgnTimezone.format("MMMM Do, YYYY");
-YgnTimeElement.innerHTML = YgnTimezone.format("hh : mm : ss");
+YgnTimeElement.innerHTML = YgnTimezone.format("h : mm : ss");
 YgnAMElement.innerHTML = YgnTimezone.format("a");
-
-BkkTimezone = moment.tz("Asia/Bangkok");
-
-let BkkTimeElement = document.querySelector("#bkkTime");
-let BkkDateElement = document.querySelector("#bkkDate");
-let BkkAMElement = document.querySelector("#bkkAM");
-BkkDateElement.innerHTML = BkkTimezone.format("MMMM Do, YYYY");
-BkkTimeElement.innerHTML = BkkTimezone.format("hh : mm : ss");
-BkkAMElement.innerHTML = BkkTimezone.format("a");
 }
 
 setInterval(updateTime,1);
+
+function addNewTimezone(event){
+    let timeZoneName = event.target.value;
+    let timeZone = moment.tz(`${timeZoneName}`);
+    let cityName = timeZoneName.replace("_", " ").split("/")[1];
+    let display = document.querySelector(".cities-container");
+    display.innerHTML += `
+    <div class="city">
+          <div class="cityInfo">
+            <h2 class="city-name">${cityName}</h2>
+            <span class="date">${timeZone.format("MMMM Do, YYYY")}</span>
+          </div>
+          <div class="city-time">
+            <h2>${timeZone.format("h : mm : ss")}</h2>
+            <span class="AM" >${timeZone.format("a")}</span>
+          </div>
+    </div>
+    `;
+}
+
+let selectedCityElement = document.querySelector("#cities");
+
+selectedCityElement.addEventListener('change', addNewTimezone);
+
